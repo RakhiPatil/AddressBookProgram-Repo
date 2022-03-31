1c3d131e96f29cccbb26b5e3fe03785cc5203b58
RakhiPatil <rakhipatil2608@gmail.com>
2022-03-31 00:06:52 -0700

    Modified

--- src/com/infogalaxy/AddressBookMain.java
+++ src/com/infogalaxy/AddressBookMain.java
@@ -1,13 +1,14 @@
 package com.infogalaxy;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBookMain {
-
-        Contact contact = new Contact();
-        Scanner scanner = new Scanner(System.in);
+        static Scanner scanner = new Scanner(System.in);
+        ArrayList<Contact> contactArrayList = new ArrayList<>();
 
         private void addContact(){
+            Contact contact = new Contact();
             System.out.println("Enter the First Name: ");
             contact.setFirstName(scanner.next());
             System.out.println("Enter the Last Name: ");
@@ -24,17 +25,23 @@ public class AddressBookMain {
             contact.setPhoneNumber(scanner.next());
             System.out.println("Enter the Email Id: ");
             contact.setEmail(scanner.next());
+            contactArrayList.add(contact);
+            System.out.println("Size of List: "+contactArrayList.size());
         }
 
         private void showContact(){
             try {
-                System.out.println(contact.toString());
+                for(int i = 0; i< contactArrayList.size(); i++){
+                    Contact contact = contactArrayList.get(i);
+                    System.out.println(contact.toString());
+                }
             }catch(NullPointerException npe){
                 System.out.println("Contact is Blank");
             }
         }
 
         private void editContact(){
+            Contact contact = new Contact();
             System.out.println("Enter the First Name: ");
             String fname = scanner.next();
             if(fname.equalsIgnoreCase(contact.getFirstName())){
@@ -60,6 +67,7 @@ public class AddressBookMain {
             }
         }
         public void deleteContact(){
+            Contact contact = new Contact();
             System.out.println("Enter the Name: ");
             String fname = scanner.next();
             if(fname.equalsIgnoreCase(contact.getFirstName())){
@@ -73,11 +81,29 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book program");
         AddressBookMain addressBookMain = new AddressBookMain();
-        addressBookMain.addContact();
-        addressBookMain.showContact();
-        addressBookMain.editContact();
-        addressBookMain.showContact();
-        addressBookMain.deleteContact();
-        addressBookMain.showContact();
+        int ch;
+        do {
+            System.out.println("1. Add Contact " + "\n" + "2.Edit Contact" + "\n" + "3.Delete Contact" + "\n" + "4.Show Contact");
+            System.out.println("Enter the choice: ");
+            ch = scanner.nextInt();
+            switch (ch) {
+                case 1:
+                    addressBookMain.addContact();
+                    break;
+
+                case 2:
+                    addressBookMain.editContact();
+                    break;
+
+                case 3:
+                    addressBookMain.deleteContact();
+                    break;
+
+                case 4:
+                    addressBookMain.showContact();
+                    break;
+            }
+        }while(ch != 5);
+
     }
 }
