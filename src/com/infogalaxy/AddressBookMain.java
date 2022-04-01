f2bf16044bf728632ef1e0569172429b0ef37a8e
RakhiPatil <rakhipatil2608@gmail.com>
2022-04-01 00:45:43 -0700

    Modified: Backup To Files

--- src/com/infogalaxy/AddressBookMain.java
+++ src/com/infogalaxy/AddressBookMain.java
@@ -1,5 +1,9 @@
 package com.infogalaxy;
-//This is Changes
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -29,7 +33,7 @@ public class AddressBookMain {
             System.out.println("Size of List: "+contactArrayList.size());
         }
 
-        private void showContact(){
+        public void showContact(){
             try {
                 for(int i = 0; i< contactArrayList.size(); i++){
                     Contact contact = contactArrayList.get(i);
@@ -41,40 +45,65 @@ public class AddressBookMain {
         }
 
         private void editContact(){
-            Contact contact = new Contact();
             System.out.println("Enter the First Name: ");
             String fname = scanner.next();
-            if(fname.equalsIgnoreCase(contact.getFirstName())){
-                System.out.println("Contact Found");
-
-                System.out.println("Enter the Last Name: ");
-                contact.setLastName(scanner.next());
-                System.out.println("Enter the Address: ");
-                contact.setAddress(scanner.next());
-                System.out.println("Enter the City Name: ");
-                contact.setCity(scanner.next());
-                System.out.println("Enter the State Name: ");
-                contact.setState(scanner.next());
-                System.out.println("Enter the Zip code: ");
-                contact.setZip(scanner.next());
-                System.out.println("Enter the Phone Number: ");
-                contact.setPhoneNumber(scanner.next());
-                System.out.println("Enter the Email Id: ");
-                contact.setEmail(scanner.next());
-
-            }else{
+            int isAvailable = 0;
+            for(int i = 0; i < contactArrayList.size(); i++) {
+                Contact contact = contactArrayList.get(i);
+                if (fname.equalsIgnoreCase(contact.getFirstName())) {
+                    isAvailable = 1;
+                    System.out.println("Contact Found");
+                    System.out.println("Enter the Last Name: ");
+                    contact.setLastName(scanner.next());
+                    System.out.println("Enter the Address: ");
+                    contact.setAddress(scanner.next());
+                    System.out.println("Enter the City Name: ");
+                    contact.setCity(scanner.next());
+                    System.out.println("Enter the State Name: ");
+                    contact.setState(scanner.next());
+                    System.out.println("Enter the Zip code: ");
+                    contact.setZip(scanner.next());
+                    System.out.println("Enter the Phone Number: ");
+                    contact.setPhoneNumber(scanner.next());
+                    System.out.println("Enter the Email Id: ");
+                    contact.setEmail(scanner.next());
+                    break;
+                }
+            }if(isAvailable == 0){
                 System.out.println("Contact Not Found");
             }
         }
         public void deleteContact(){
-            Contact contact = new Contact();
             System.out.println("Enter the Name: ");
             String fname = scanner.next();
-            if(fname.equalsIgnoreCase(contact.getFirstName())){
-                System.out.println("Contact Found");
-                contact = null ;
-            }else{
+            int isAvailable  = 0;
+            for(int i = 0; i < contactArrayList.size(); i++) {
+                Contact contact = contactArrayList.get(i);
+                if (fname.equalsIgnoreCase(contact.getFirstName())) {
+                    isAvailable = 1;
+                    System.out.println("Contact Found");
+                    contactArrayList.remove(i);
+                    break;
+                }
+            }if(isAvailable == 0){
                 System.out.println("Contact Not Found");
+            }
+        }
+        public void backupToFile(){
+            try {
+                String contactData = null;
+                for (int i = 0; i < contactArrayList.size(); i++) {
+                    Contact contact = contactArrayList.get(i);
+                    contactData = contact.getFirstName() + "," + contact.getLastName() + "," + contact.getAddress() + "," + contact.getCity() + "," + contact.getState() + "," + contact.getZip() + "," + contact.getPhoneNumber() + "," + contact.getEmail() + "\n" + contactData;
+                }
+                Path file = Paths.get("MyAddressBook.txt");
+                byte[] byteData = contactData.getBytes();
+                Files.write(file, byteData);
+            }
+            catch(NullPointerException npe){
+                System.out.println("Contact is Blank.");
+            }catch(IOException e){
+                e.printStackTrace();
             }
         }
 
@@ -83,7 +112,7 @@ public class AddressBookMain {
         AddressBookMain addressBookMain = new AddressBookMain();
         int ch;
         do {
-            System.out.println("1. Add Contact " + "\n" + "2.Edit Contact" + "\n" + "3.Delete Contact" + "\n" + "4.Show Contact");
+            System.out.println("1. Add Contact " + "\n" + "2.Edit Contact" + "\n" + "3.Delete Contact" + "\n" + "4.Show Contact"+"\n"+"5.Backup To Files");
             System.out.println("Enter the choice: ");
             ch = scanner.nextInt();
             switch (ch) {
@@ -102,8 +131,11 @@ public class AddressBookMain {
                 case 4:
                     addressBookMain.showContact();
                     break;
+                case 5:
+                    addressBookMain.backupToFile();
+                    break;
             }
-        }while(ch != 5);
+        }while(ch != 6);
 
     }
 }
